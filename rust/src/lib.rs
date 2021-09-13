4cda0daec8bfae21df51cf9f3b75a5110ace708f
uselessgoddess <dodickgod@gmail.com>
2021-09-13 21:26:26 +0300

    CrUD example like C

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(try_trait)]
+#![feature(in_band_lifetimes)]
 
 use common::{*};
 use link_c::{*};
@@ -16,6 +17,7 @@ mod test {
 
     #[test]
     fn it_works() {
+        std::fs::remove_file("db.links");
         {
             let mem = Links::create("db.links");
             println!("{:?}", mem);
@@ -25,5 +27,28 @@ mod test {
         let mem = Links::open("db.links").unwrap();
         println!("{:?}", mem);
         mem.close().unwrap();
+    }
+
+    #[test]
+    fn CrUD() {
+        std::fs::remove_file("db.links");
+        let mem = Links::create("db.links").unwrap();
+
+        let itself = 0;
+
+        let mut is_a = mem.create(itself, itself, itself);
+        let is_not_a = mem.create(itself, itself, &is_a);
+        let link = mem.create(itself, &is_a, itself);
+        let thing = mem.create(itself, &is_not_a, &link);
+
+        let clone = is_a.clone();
+        is_a.update(&clone, &clone, &link);
+        // or
+        // is_a.update(is_a.index(), is_a.index(), &link);
+
+        is_a.delete(); // delete all
+        thing.delete();
+
+        println!("{:?}", mem.count());
     }
 }
--- rust/src/persistent_memory_manager.rs
+++ rust/src/persistent_memory_manager.rs
@@ -1,9 +1,103 @@
-use crate::common::{*};
-use crate::persistent_memory_manager_c::{*};
-use libc::c_char;
-use std::path::Path;
-use std::io::{Error, ErrorKind};
 use std::ffi::{CStr, CString, NulError};
+use std::io::{Error, ErrorKind};
+use std::path::Path;
+
+use libc::c_char;
+
+use crate::common::{*};
+use crate::link_c::{*};
+use crate::persistent_memory_manager_c::{*};
+use std::ops::Deref;
+
+pub enum LinkKind {
+    Source,
+    Linker,
+    Target,
+}
+
+pub struct LinkAdapter<'a> {
+    parent: &'a PersistentMemoryManager,
+    index: LinkIndex,
+}
+
+impl<'a> Clone for LinkAdapter<'a> {
+    fn clone(&self) -> Self {
+        Self { parent: self.parent, index: self.index }
+    }
+}
+
+pub trait AsIndex {
+    fn index(&self) -> LinkIndex;
+}
+
+impl AsIndex for LinkIndex {
+    fn index(&self) -> LinkIndex {
+        *self
+    }
+}
+
+impl<'a> AsIndex for LinkAdapter<'a> {
+    fn index(&self) -> LinkIndex {
+        self.index
+    }
+}
+
+impl<'a> AsIndex for &LinkAdapter<'a> {
+    fn index(&self) -> LinkIndex {
+        self.index
+    }
+}
+
+impl<'a> Deref for LinkAdapter<'a> {
+    type Target = LinkIndex;
+
+    fn deref(&self) -> &Self::Target {
+        &self.index
+    }
+}
+
+impl<'a> LinkAdapter<'a> {
+    // pub fn first_referer_by(&self, kind: LinkKind) -> Self {
+    //     match kind {
+    //         LinkKind::Source => unsafe {
+    //             LinkAdapter {
+    //                 parent: self.parent,
+    //                 index: GetFirstRefererBySourceIndex(self.index),
+    //             }
+    //         }
+    //         LinkKind::Linker => unsafe {
+    //             LinkAdapter {
+    //                 parent: self.parent,
+    //                 index: GetFirstRefererByLinkerIndex(self.index),
+    //             }
+    //         }
+    //         LinkKind::Target => unsafe {
+    //             LinkAdapter {
+    //                 parent: self.parent,
+    //                 index: GetFirstRefererByTargetIndex(self.index),
+    //             }
+    //         }
+    //     }
+    // }
+
+    pub fn update<S, L, T>(&mut self, source: T, linker: L, target: S)
+        where S: AsIndex, L: AsIndex, T: AsIndex
+    {
+        self.index = unsafe { UpdateLink(self.index, source.index(), linker.index(), target.index()) }
+    }
+
+    pub fn delete(self)  {
+        unsafe { DeleteLink(self.index) }
+    }
+
+    // pub fn replace<O: AsIndex>(&mut self, other: O) {
+    //     self.index = unsafe { CreateLink(source.index(), linker.index(), target.index()) },
+    // }
+
+    // pub fn drop_index(self) -> LinkIndex {
+    //     self.index()
+    // }
+}
 
 pub struct Links;
 
@@ -21,14 +115,14 @@ impl Links {
         let raw_path_str = path.as_ref().to_str();
         let path_str;
         match raw_path_str {
-            None => { return Err(Error::from(ErrorKind::NotFound)) }
+            None => { return Err(Error::from(ErrorKind::NotFound)); }
             Some(str) => { path_str = str }
         }
 
         let c_str = CString::new(path_str)?;
         let result = unsafe { OpenLinks(c_str.as_ptr()) };
         match result {
-            SUCCESS_RESULT => { Ok(PersistentMemoryManager { }) }
+            SUCCESS_RESULT => { Ok(PersistentMemoryManager {}) }
             _ => { Err(Error::from_raw_os_error(result as i32)) }
         }
     }
@@ -47,6 +141,20 @@ impl Links {
 }
 
 impl PersistentMemoryManager {
+    pub fn create<S, L, T>(&'a self, source: S, linker: L, target: T) -> LinkAdapter<'a>
+        where S: AsIndex, L: AsIndex, T: AsIndex
+    {
+        LinkAdapter { parent: self, index: unsafe { CreateLink(source.index(), linker.index(), target.index()) } }
+    }
+
+    // pub fn search(&'a self, source: LinkIndex, linker: LinkIndex, target: LinkIndex) -> LinkAdapter<'a> {
+    //     LinkAdapter { parent: self, index: unsafe { SearchLink(source, linker, target) } }
+    // }
+
+    pub fn count(&self) -> usize {
+        (unsafe { GetLinksCount() }) as usize
+    }
+
     pub fn close(&self) -> std::io::Result<()> {
         let result = unsafe { CloseLinks() };
         match result {
